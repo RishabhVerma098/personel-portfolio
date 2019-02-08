4c2f768104b3b1661bc01ab09f5922ff2927a733
rishabh verma <vermarishabh0987@gmail.com>
2019-02-08 21:07:10 +0530

    jobs part done !!

--- job/views.py
+++ job/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
+from .models import jobs
 
 
 def home(request):
-    return render(request, 'job/home.html')
+    job = jobs.objects
+    return render(request, 'job/home.html', {'jobs': job})
