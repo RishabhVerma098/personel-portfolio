3d8365cefe3aeebe6a14fbed459c65a2c23ebc9a
rishabh verma <vermarishabh0987@gmail.com>
2019-02-08 19:13:50 +0530

    created (intialized) home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class blog(models.Model):
+    title = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
