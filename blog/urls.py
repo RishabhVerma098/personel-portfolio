c11aca5b878a5d7490c7159f3b447a45013a5d04
rishabh verma <vermarishabh0987@gmail.com>
2019-02-09 19:58:20 +0530

    site completed !!

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.allblogs, name='allblogs'),
+    path('<int:Blog_ID>/', views.details, name='details')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import blog
 # Create your views here.
 
@@ -6,3 +6,9 @@ from .models import blog
 def allblogs(request):
     blogs = blog.objects
     return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+def details(request,Blog_ID):
+    blogs = get_object_or_404(blog,pk = Blog_ID)
+    return render(request,'blog/detailblog.html',{'blogs':blogs})
+    
+
